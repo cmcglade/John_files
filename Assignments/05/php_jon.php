46079114078c2d38c863a3919a6ca146e4cfd073
jonliu <jonliu417@gmail.com>
2015-11-20 02:07:07 -0500

    make changes for'php_jon.php' to show a different color with the inputting contents

--- Assignments/05/php_jon.php
+++ Assignments/05/php_jon.php
@@ -10,7 +10,7 @@
 <body>
 
   <h2>A paragraph with PHP</h2>
-  
+
   <!--  the form  -->
   <h3>Input what you want to see in paragraph</h3>
   <form method="POST">
@@ -34,9 +34,14 @@
     $last_name = ucwords($_POST["last_name"]);
     $maiden_name = ucwords($_POST["mother_maiden_name"]);
     $fav_color = $_POST["favourite_color"];
-    $postal_code = $_POST["postal_code"];
+    $postal_code = strtoupper($_POST["postal_code"]);
     echo "Hi $first_name!, I find it interesting to think about how our identity might  change if we had our mother's name. If your name wasn't $first_name $last_name, but instead it was $first_name $maiden_name, would your favourite colour still be $fav_color? Would you still live in $postal_code? ";
    ?>
+
+   <!-- try to show the input with a different color -->
+   <p>
+      Hi <span style="color:red"><?php echo $first_name; ?></span>!, I find it interesting to think about how our identity might  change if we had our mother's name. If your name wasn't <span style="color:red"><?php echo $first_name; ?></span> <span style="color:red"><?php echo $last_name ?></span> , but instead it was <span style="color:red"><?php echo $first_name; ?></span> <span style="color:red"><?php echo $maiden_name ?></span>, would your favourite colour still be <span  style="color:red"><?php echo $fav_color; ?>? </span> Would you still live in <span style="color:red"><?php echo $postal_code; ?></span>?
+   </p>
 
 </body>
 
